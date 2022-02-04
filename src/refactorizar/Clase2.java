d661a8db17b9a8b09a052425e509332364bd6551
dam <dam@203P01>
2022-02-04 12:10:10 +0100

    SEGUNDO COMMIT

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public Clase2(){
+        System.out.println("Linea 1");
+        System.out.println("Linea 2s");
+    }
+}
