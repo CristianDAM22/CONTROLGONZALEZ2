7dd401aa4d43e024f792c7a1545b6f7158a852e5
dam <dam@203P01>
2022-02-04 12:07:15 +0100

    PRIMER COMMIT

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,11 @@
+
+package refactorizar;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("LINEA 1");
+        System.out.println("Hola mundos");
+    }
+    
+}
